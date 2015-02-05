81778f4afc4e39137fedfdba3500b5630b880f12
RegisOB <obianregi@yahoo.fr>
2015-02-05 15:16:08 +0100

    adding R function for checking the extracted data

--- R/GabSaniR.R
+++ R/GabSaniR.R
@@ -599,9 +599,110 @@ InfoSaniR_report <- function(data = "patho", Year = 2000, file = FALSE) {
     file.copy(from = rep, to = fileReport, overwrite = T)
     file.copy(from = min, to = fileReport, overwrite = T)
     setwd(fileReport)
-    knit("patho.Rnw", encoding = "UTF-8", quiet = F)
-    texi2pdf("patho.tex", clean = T)
+    knit2pdf("patho.Rnw", encoding = "UTF-8", quiet = F)
+    knit2pdf("patho.Rnw", encoding = "UTF-8", quiet = T)
+  
+    #texi2pdf("patho.tex")
+    #texi2dvi("patho.tex", pdf=T, clean = T)
 } 
+InfoSaniR_check <- function(data = "patho", Year = 2000, file = FALSE) {
+  ## Checking inputs
+  if (missing(data) && file == FALSE) {
+    stop("data doit etre specifie car manquant")
+  }
+  if (!is.character(data) && file != FALSE) {
+    stop("data doit etre un caractere")
+  }
+  
+  if (missing(Year)) {
+    stop("Year doit etre specifie car manquant")
+  }
+  if (!is.numeric(Year)) {
+    stop("Year doit etre de type numerique")
+  }
+  if (Year < 2000 | Year > 2099) {
+    stop("Year doit etre compris entre 2000 et 2099")
+  }
+  
+  if (exists(data, envir = .GlobalEnv) && file == FALSE) {
+    setwd("~")
+    if (is.element(paste("Verification", data, as.character(Year), 
+      sep = "_"), list.files(getwd()))) {
+      fileReport <- normalizePath(file.path(getwd(), 
+        paste("Verification", data, as.character(Year), sep = "_")))
+    } else {
+      dir.create(file.path(getwd(), paste("Verification", data, 
+        as.character(Year), sep = "_")))
+      fileReport <- normalizePath(file.path(getwd(), 
+        paste("Verification", data, as.character(Year), sep = "_")))
+    }
+    
+  } else {
+    # Choosing a folder interactively
+    fileExtract <- choose.dir(default = "", caption = "Le dossier donnees .RData InfoSani")
+    fileReport <- choose.dir(default = "", caption = "Le dossier des verifications final")
+    
+    ## Checking extracted data files specifier la recherche
+    ## selon data mis en entree (patho,....All)
+    FileData <- list.files(path = fileExtract, pattern = ".RData$")
+    DirFileData <- as.list(dir(path = fileExtract, pattern = ".RData$", 
+      full.names = T))
+    if (length(FileData) < 1L) {
+      stop("le dossier ne contient pas les fichiers format .RData ou mal specifie")
+    }
+    
+    # Checking the year
+    year <- str_extract(FileData, "20[0-9]{2}")
+    year_bad <- FileData[!is.element(year, as.character(Year))]
+    if (length(year_bad) > 0) {
+      cat("Le(s) annee(s) de(s) fichier(s) ci-dessous est(sont) mal specifie(s) \n          ou l'annee d'extraction ne corresponds pas a ceux des fichiers:", 
+        "\n")
+      print(year_bad)
+      stop("Veuillez corriger avant de continuer svp")
+    }
+    
+    # Loading all Data.RData
+    lapply(DirFileData, load, envir = .GlobalEnv)
+  }
+  
+  ################################################################################
+  data0 <- patho
+  L0 <- c('Province', 'Region_Sanitaire', 'Departement', 'Etablissement',
+    'Nom', 'Maladies', 'Code_Maladie','Annee', 'Mois', 'Effectif')
+  ListMistak0 <- which(is.na(as.numeric(as.character(data0$Effectif))))
+  Mistak0 <- data0[ListMistak1, L0]
+  
+  if (dim(Mistak0)[1]>1) {
+    tab0 <- xtable((Mistak0),
+      caption="Les erreurs de saisies repertoriées",label="tab0",digits=0)
+    print(tab0, include.rownames = FALSE)
+    write.xlsx(Mistak0, file = file.path(fileOutput,paste(paste('data_patho', year[1], sep='_'),'xlsx',sep='.')),
+      row.names = F)
+  }
+  
+  ####
+  Mistak1 <- ddply(data0,.(Province, Region_Sanitaire, Departement, Etablissement, Nom, Maladies, Domaine, Annee,Mois),summarise,N=sum(na.omit(as.numeric(as.character(Effectif)))))
+  #Fréquence des cas mensuelles de Maladiess
+  
+  Mistak2 <- ddply(Mistak1,.(Province, Region_Sanitaire, Departement, Etablissement, Nom, Maladies, Domaine, Annee),summarise,
+    MAD=round(mad(na.omit(N)),0),
+    MED=round(median(na.omit(N)),0)) 
+  #Mediane absolue et mediane annuelle des cas de Maladiess
+  
+  Mistak3 <- merge(Mistak1,Mistak2)
+  Mistake_Infectious <-subset(Mistak3,N-MED>5*MAD&N-MED>50&Domaine=='Infect')
+  Mistake_NoInfectious <-subset(Mistak3,N-MED>5*MAD&N-MED>50&Domaine!='Infect')
+  
+  if (dim(Mistake_Infectious)[1]>1) {
+    tab0 <- xtable((Mistake_Infectious),
+      caption="Les erreurs de saisies repertoriées",label="tab0",digits=0)
+    print(tab0, include.rownames = FALSE)}
+  
+  ##############################################################################################
+  
+  
+}
+
 
 
 
